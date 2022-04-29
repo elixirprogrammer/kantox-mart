0f2912c7b5c903b4008390da7f7dc2b1974949b4
elixirprogrammer <anthony@elixirprogrammer.com>
2022-04-29 01:45:13 -0400

    adds pricing rules tests

--- lib/pricing_rules.ex
+++ lib/pricing_rules.ex
@@ -3,55 +3,71 @@ defmodule PrincingRules do
   Defined to apply pricing rules
   """
 
-  def apply_offers(price_amounts_list) do
-    price_amounts_list
+  @doc """
+  Applies defined offers based on prices list.
+
+  ## Examples
+
+      iex> PrincingRules.apply_offers([3.11, 3.11])
+      [3.11]
+
+      iex> PrincingRules.apply_offers([5.00, 5.00, 5.00])
+      [4.50, 4.50, 4.50]
+
+      iex> PrincingRules.apply_offers([11.23, 11.23, 11.23])
+      [7.486666666666667, 7.486666666666667, 7.486666666666667]
+
+  """
+  @spec apply_offers(prices_list :: list()) :: list()
+  def apply_offers(prices_list) do
+    prices_list
     |> special_condition(:green_tea)
     |> special_condition(:strawberry)
     |> special_condition(:coffee)
   end
 
-  defp special_condition(price_amounts_list, :green_tea) do
-    green_tea_quantity = Enum.count(price_amounts_list, &(&1 == 3.11))
+  defp special_condition(prices_list, :green_tea) do
+    green_tea_quantity = Enum.count(prices_list, &(&1 == 3.11))
 
-    green_tea_offer(green_tea_quantity, price_amounts_list)
+    green_tea_offer(green_tea_quantity, prices_list)
   end
 
-  defp special_condition(price_amounts_list, :strawberry) do
-    strawberry_quantity = Enum.count(price_amounts_list, &(&1 == 5.00))
+  defp special_condition(prices_list, :strawberry) do
+    strawberry_quantity = Enum.count(prices_list, &(&1 == 5.00))
 
-    strawberry_offer(strawberry_quantity, price_amounts_list)
+    strawberry_offer(strawberry_quantity, prices_list)
   end
 
-  defp special_condition(price_amounts_list, :coffee) do
-    coffee_quantity = Enum.count(price_amounts_list, &(&1 == 11.23))
+  defp special_condition(prices_list, :coffee) do
+    coffee_quantity = Enum.count(prices_list, &(&1 == 11.23))
 
-    coffee_offer(coffee_quantity, price_amounts_list)
+    coffee_offer(coffee_quantity, prices_list)
   end
 
-  defp green_tea_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity > 1 do
-    List.delete(price_amounts_list, 3.11)
+  defp green_tea_offer(green_tea_quantity, prices_list) when green_tea_quantity > 1 do
+    List.delete(prices_list, 3.11)
   end
 
-  defp green_tea_offer(_green_tea_quantity, price_amounts_list) do
-    price_amounts_list
+  defp green_tea_offer(_green_tea_quantity, prices_list) do
+    prices_list
   end
 
-  defp strawberry_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity >= 3 do
-    price_amounts_list
+  defp strawberry_offer(green_tea_quantity, prices_list) when green_tea_quantity >= 3 do
+    prices_list
     |> Enum.map(& drop_strawberry_price(&1))
   end
 
-  defp strawberry_offer(_green_tea_quantity, price_amounts_list) do
-    price_amounts_list
+  defp strawberry_offer(_green_tea_quantity, prices_list) do
+    prices_list
   end
 
-  defp coffee_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity >= 3 do
-    price_amounts_list
+  defp coffee_offer(green_tea_quantity, prices_list) when green_tea_quantity >= 3 do
+    prices_list
     |> Enum.map(& drop_coffee_price(&1))
   end
 
-  defp coffee_offer(_green_tea_quantity, price_amounts_list) do
-    price_amounts_list
+  defp coffee_offer(_green_tea_quantity, prices_list) do
+    prices_list
   end
 
   defp drop_strawberry_price(price) when price == 5.00, do: 4.50
