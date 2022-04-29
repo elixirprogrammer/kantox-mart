45f9d25765b2d59b7cee2342c100b0473d70c252
elixirprogrammer <anthony@elixirprogrammer.com>
2022-04-29 00:24:34 -0400

    adds strawberry and cofee pricing rules

--- lib/pricing_rules.ex
+++ lib/pricing_rules.ex
@@ -6,12 +6,26 @@ defmodule PrincingRules do
   def apply_offers(price_amounts_list) do
     price_amounts_list
     |> special_condition(:green_tea)
+    |> special_condition(:strawberry)
+    |> special_condition(:coffee)
   end
 
   defp special_condition(price_amounts_list, :green_tea) do
     green_tea_quantity = Enum.count(price_amounts_list, &(&1 == 3.11))
 
     green_tea_offer(green_tea_quantity, price_amounts_list)
+  end
+
+  defp special_condition(price_amounts_list, :strawberry) do
+    strawberry_quantity = Enum.count(price_amounts_list, &(&1 == 5.00))
+
+    strawberry_offer(strawberry_quantity, price_amounts_list)
+  end
+
+  defp special_condition(price_amounts_list, :coffee) do
+    coffee_quantity = Enum.count(price_amounts_list, &(&1 == 11.23))
+
+    coffee_offer(coffee_quantity, price_amounts_list)
   end
 
   defp green_tea_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity > 1 do
@@ -21,4 +35,28 @@ defmodule PrincingRules do
   defp green_tea_offer(_green_tea_quantity, price_amounts_list) do
     price_amounts_list
   end
+
+  defp strawberry_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity >= 3 do
+    price_amounts_list
+    |> Enum.map(& drop_strawberry_price(&1))
+  end
+
+  defp strawberry_offer(_green_tea_quantity, price_amounts_list) do
+    price_amounts_list
+  end
+
+  defp coffee_offer(green_tea_quantity, price_amounts_list) when green_tea_quantity >= 3 do
+    price_amounts_list
+    |> Enum.map(& drop_coffee_price(&1))
+  end
+
+  defp coffee_offer(_green_tea_quantity, price_amounts_list) do
+    price_amounts_list
+  end
+
+  defp drop_strawberry_price(price) when price == 5.00, do: 4.50
+  defp drop_strawberry_price(price), do: price
+
+  defp drop_coffee_price(price) when price == 11.23, do: 11.23 / 3 * 2
+  defp drop_coffee_price(price), do: price
 end
--- test/kantox_mart_test.exs
+++ test/kantox_mart_test.exs
@@ -14,13 +14,13 @@ defmodule KantoxMartTest do
   end
 
   test "checks out with each product" do
-    KantoxMart.add_to_basket(["GR1", "SR1", "CR1"])
+    KantoxMart.add_to_basket(["GR1", "SR1", "CF1"])
 
     assert "£19.34" = KantoxMart.get_checkout_total()
   end
 
   test "resets basket after checkout" do
-    KantoxMart.add_to_basket(["GR1", "SR1", "CR1"])
+    KantoxMart.add_to_basket(["GR1", "SR1", "CF1"])
 
     assert "£19.34" = KantoxMart.get_checkout_total()
 
@@ -33,19 +33,21 @@ defmodule KantoxMartTest do
     assert "£3.11" = KantoxMart.get_checkout_total()
   end
 
-  @tag :pending
   test "checks out with strawberry offer" do
     KantoxMart.add_to_basket(["SR1", "SR1", "GR1", "SR1"])
+
     assert "£16.61" = KantoxMart.get_checkout_total()
   end
-  @tag :pending
+
   test "checks out with cofee offer" do
     KantoxMart.add_to_basket(["GR1", "CF1", "SR1", "CF1", "CF1"])
+
     assert "£30.57" = KantoxMart.get_checkout_total()
   end
-  @tag :pending
+
   test "checks out with available offers" do
     KantoxMart.add_to_basket(["GR1", "SR1", "GR1", "GR1", "CF1"])
+
     assert "£22.45" = KantoxMart.get_checkout_total()
   end
 end
