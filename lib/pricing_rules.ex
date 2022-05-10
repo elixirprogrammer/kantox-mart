5be73786592d4c06d5299d7153ff2fea9887c27e
elixirprogrammer <anthony@elixirprogrammer.com>
2022-05-10 14:55:03 -0400

    adds prices attributes to pricing rules

--- lib/pricing_rules.ex
+++ lib/pricing_rules.ex
@@ -5,6 +5,15 @@ defmodule PrincingRules do
 
   @offers [:green_tea, :strawberries, :coffee]
 
+  # Products Prices
+  @green_tea_price 3.11
+  @strawberries_price 5.0
+  @coffee_price 11.23
+
+  # Products Dicount Prices
+  @strawberries_price_discount 4.50
+  @coffee_price_discount @coffee_price / 3 * 2
+
   @doc """
   Applies defined offers for each product inside basket list, then updates the basket list state
   when offers applied and price updated, and returns the basket.
@@ -40,7 +49,7 @@ defmodule PrincingRules do
   # Removes a green tea product from basket list if more than one is bought
   defp special_condition(:green_tea) do
     basket_list = Basket.get()
-    green_tea_quantity = Enum.count(basket_list, &(&1.price == 3.11))
+    green_tea_quantity = Enum.count(basket_list, &(&1.price == @green_tea_price))
 
     if green_tea_quantity > 1, do: green_tea_offer(basket_list)
   end
@@ -48,7 +57,7 @@ defmodule PrincingRules do
   # Drops strawberry price to 4.50 for each product in basket list if 3 or more bought
   defp special_condition(:strawberries) do
     basket_list = Basket.get()
-    strawberries_quantity = Enum.count(basket_list, &(&1.price == 5.00))
+    strawberries_quantity = Enum.count(basket_list, &(&1.price == @strawberries_price))
 
     if strawberries_quantity >= 3, do: strawberries_offer(basket_list)
   end
@@ -56,14 +65,14 @@ defmodule PrincingRules do
   # Drops coffee price to two thirds for each product in basket list if 3 or more bought
   defp special_condition(:coffee) do
     basket_list = Basket.get()
-    coffee_quantity = Enum.count(basket_list, &(&1.price == 11.23))
+    coffee_quantity = Enum.count(basket_list, &(&1.price == @coffee_price))
 
     if coffee_quantity >= 3, do: coffee_offer(basket_list)
   end
 
   defp green_tea_offer(basket_list) do
     basket_list
-    |> List.delete(%Product{code: "GR1", name: "Green tea", price: 3.11})
+    |> List.delete(%Product{code: "GR1", name: "Green tea", price: @green_tea_price})
     |> Basket.update()
   end
 
@@ -79,9 +88,13 @@ defmodule PrincingRules do
     |> Basket.update()
   end
 
-  defp drop_strawberries_price(product) when product.price == 5.00, do: %{product | price: 4.50}
+  defp drop_strawberries_price(product) when product.price == @strawberries_price do
+    %{product | price: @strawberries_price_discount}
+  end
   defp drop_strawberries_price(product), do: product
 
-  defp drop_coffee_price(product) when product.price == 11.23, do: %{product | price: 11.23 / 3 * 2}
+  defp drop_coffee_price(product) when product.price == @coffee_price do
+    %{product | price: @coffee_price_discount}
+  end
   defp drop_coffee_price(product), do: product
 end
